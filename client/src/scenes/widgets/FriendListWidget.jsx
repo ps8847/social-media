e06487a847657662355523b5e88c13fe5d7742fa
Prince Sharma <ps789650@gmail.com>
2024-04-14 18:28:56 +0530

    updated server

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://social-media-78fq.onrender.comusers/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`http://localhost:3001/posts`, {
+    const response = await fetch(`https://social-media-78fq.onrender.composts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
